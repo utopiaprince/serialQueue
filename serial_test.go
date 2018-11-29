a0afff272b422ae962ea55cfa02afc6ebdfeb8ec
utopiaprince <gang.cheng1987@gmail.com>
2018-11-29 08:53:55 +0800

    out_1129_modify: 添加测试用例

--- serial_test.go
+++ serial_test.go
@@ -48,6 +48,7 @@ func TestSerial_Frame_Ed(t *testing.T){
 	mySerial.Write(originBuf)
 }
 
+
 func TestSerialFrame_St_Ed(t *testing.T) {
 	mySerial, _ := New(SerialReg{
 		St: SerialStart{
@@ -106,6 +107,39 @@ func TestSerialFrame_St_Ld(t *testing.T) {
 		0x01,0x02,0x00,0x02,3,4,
 		2,3,4,0x0d,
 		0x01,0x02,0x00,0x04,0x0a,0x0b,0x0c,0x0d}
+	beego.Info("write buf: ", hex.EncodeToString(originBuf))
+	mySerial.Write(originBuf)
+}
+
+func TestSerialFrame_St_Ld_Offset(t *testing.T) {
+	mySerial, _ := New(SerialReg{
+		St: SerialStart{
+			valid: true,
+			data: []uint8{0x01},
+		},
+		Ld: SerialLenDesc{
+			valid: true,
+			pos: 2,
+			len: 2,
+		},
+		Argu: SerialArgu{
+			lenMax: 16,
+			lenMin: 2,
+		},
+	}, 1024)
+	mySerial.AddCallback(func() {
+		beego.Info("mySerial len: ", mySerial.FrmLen())
+		readBuf, _ := mySerial.Read(mySerial.FrmLen())
+		beego.Info("read buf: ", hex.EncodeToString(readBuf))
+	})
+
+	if mySerial == nil {
+		t.Errorf("Serial New failed")
+	}
+	originBuf := []byte{
+		0x01,0x03,0x00,0x02,3,4,
+		2,3,4,0x0d,
+		0x01,0x08,0x00,0x04,0x0a,0x0b,0x0c,0x0d}
 	beego.Info("write buf: ", hex.EncodeToString(originBuf))
 	mySerial.Write(originBuf)
 }
